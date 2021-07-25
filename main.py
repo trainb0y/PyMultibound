df32023bf82c51961cd06749cd1689e541cb3ae9
trainb0y1 <btbevo00@gmail.com>
2021-07-25 16:03:02 -0600

    Implement basic functionality

--- main.py
+++ main.py
@@ -17,11 +17,32 @@ from colorama import Style, Fore, Back, init
 init(autoreset=True) # Should make this work for more windows use cases
                      # also will remove the need to reset color after each line.
 
-steamapps_dir = os.path.join(os.path.expanduser("~"),"/Program Files (x86)/Steam/steamapps")
-profiles = [Profile(),Profile()]
-current_profile = profiles[0] # temporary
-profiles[0].name = "test1"
-profiles[1].name = "test2"
+steamapps_dir = os.path.join("c:\\","Program Files (x86)","Steam","steamapps") # see docs on os.path.join 
+starbound_dir = os.path.join(steamapps_dir,"common","Starbound")
+print(starbound_dir)
+
+profiles = []
+
+# Iterate through profiles and create them
+profiles_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),"profiles")
+for name in next(os.walk(profiles_dir))[1]:
+    prof = Profile()
+    prof.create(name,starbound_dir)
+    profiles.append(prof)
+
+# If we don't have a vanilla profile, make one
+vanilla_exists = False
+for prof in profiles:
+    if prof.name == "Vanilla":
+        vanilla_exists = True
+
+if not vanilla_exists:
+    vanilla = Profile()
+    vanilla.create("Vanilla",starbound_dir)
+
+current_profile = profiles[0]
+
+
 
 def select_profile(profiles):
     """Menu screen, returns a profile from the list of profiles provided"""
@@ -51,24 +72,30 @@ def load_profile():
     global current_profile
     """Switch the currently loaded profile for a different one"""
     # Have the user select a profile and then load it in
-    profile = select_profile(profiles)
-    if "y" in input(f"{Fore.GREEN}Load profile {profile.name}? {Fore.YELLOW}This will erase the current game data!\n"+
+    # profile = select_profile(profiles)
+    if "y" in input(f"{Fore.GREEN}Load profile {current_profile.name}? {Fore.YELLOW}This will erase the current game data!\n"+
         "(Save it as a profile/update the profile first) (Y/N) ").lower():
-        profile.load()
-        current_profile = profile
+        current_profile.load()
     else: print(f"\n{Fore.YELLOW}Profile load aborted")
+
+
+def switch_profile():
+    """Switch current profile but do no file manipulation"""
+    global current_profile
+    current_profile = select_profile(profiles)
+
     
 
 def new_profile():
     """Create a new profile"""
     name = input("Enter profile name: ")
     profile = Profile()
-    profile.create(name, steamapps_dir)
+    profile.create(name, starbound_dir)
     profiles.append(profile)
     
 def edit_profile(): 
     """Edit profile menu (for changing name, etc)"""
-    pass
+    print(f"{Fore.MAGENTA}Profile editing menu coming soon!")
 
 
 def delete_profile():
@@ -84,22 +111,22 @@ def delete_profile():
 
 def help_page():
     """Show a basic description of the options"""
-    print(
-"""
-Yeah... help text comes later
-"""
-    )
+    print(f"{Fore.MAGENTA}Yeah... help text comes later")
 
 def run_starbound():
     """Run starbound as it is now, and update the current profile on exit"""
     global current_profile
-    # OS.path.join does not escape spaces, so
-    # it things you are trying to run
+    load_profile()
+    cmd = os.path.join(starbound_dir,"win64","starbound.exe")
+    # os.path.join() does not escape spaces, so
+    # it thinks you are trying to run
     # C:/Program beacause of the space in program files
     # So, have to do some string work lol
-    cmd = os.path.join(steamapps_dir,"common/Starbound/win64/starbound.exe")
     os.system(f'"{cmd}"')  # Run the game
+    print(f"{Fore.GREEN}Updating profile, please wait...")
+    print(f"{Fore.YELLOW}This may take a while if you have a large universe or many mods")
     current_profile.update()
+    print(f"{Fore.GREEN}Profile {current_profile.name} updated!")
 
 
 if __name__ == "__main__":
@@ -111,7 +138,7 @@ if __name__ == "__main__":
         "Main Menu - Please Select an Option", [
         ("Help",help_page),
         (f"Run Starbound ({current_profile.name})",run_starbound),
-        ("Load Profile", load_profile),
+        ("Switch Profile", switch_profile),
         (f"Update Profile ({current_profile.name})", current_profile.update),
         ("New Profile", new_profile),
         ("Edit Profile", edit_profile),
--- profile.py
+++ profile.py
@@ -2,28 +2,45 @@ import os, shutil
 
 class Profile():
 
-    def create(self,name, steamapps_dir):
+    def create(self,name, starbound_dir):
         # Get the directory of this script
         script_dir = os.path.dirname(os.path.realpath(__file__))
         profiles_dir = os.path.join(script_dir,"profiles")
-        # Find the saves directory, if it doesn't exist, make it
-        if not os.path.exists(profiles_dir):
-            os.makedirs(profiles_dir)
 
         # Now we make a directory for this particular profile, inside of the saves dir
         profile_dir = os.path.join(profiles_dir,name)
-        os.mkdir(profile_dir)
+        
+        for directory in [os.path.join(profile_dir,"mods"),os.path.join(profile_dir,"storage")]:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
 
         self.name = name # Save the name
         self.directory = profile_dir
-        self.steamapps_dir = steamapps_dir
+        self.starbound_dir = starbound_dir
+        
+
+    def clear_starbound(self):
+        """Delete all of the profile specific stuff in the starbound folder"""
+        for directory in ["mods","storage"]:
+            if os.path.exists(os.path.join(self.starbound_dir,directory)):
+                shutil.rmtree(os.path.join(self.starbound_dir,directory))
+            #os.makedirs(os.path.join(self.starbound_dir,directory))
+
 
 
     def load(self):
         """Load this profile into the starbound install"""
+        self.clear_starbound()
+        for directory in ["mods","storage"]:
+            shutil.move(os.path.join(self.directory,directory),self.starbound_dir)
+
 
     def update(self):
         """Update this profile with the current starbound data"""
+        if os.path.exists(self.directory):
+            shutil.rmtree(self.directory)
+        shutil.move(os.path.join(self.starbound_dir,"mods"),os.path.join(self.directory,"mods"))
+        shutil.move(os.path.join(self.starbound_dir,"storage"),os.path.join(self.directory,"storage"))
 
     def delete(self):
         """Delete all files relating to this profile"""
