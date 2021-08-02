b95bcf692637a01c382412afe04c31d1013684ee
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-08-02 10:10:01 -0600

    Improve settings.json formatting

--- settingsloader.py
+++ settingsloader.py
@@ -35,7 +35,7 @@ if not os.path.isfile(os.path.join(os.path.dirname(os.path.realpath(__file__)), 
             "colored-text": True,
             "steamapps-directory": ("c:\\", "Program Files (x86)", "Steam", "steamapps"),
             "compress-profiles": True
-        }, f)
+        }, f, indent=4)
 
 
 def load_settings():
