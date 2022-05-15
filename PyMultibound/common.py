8833c85648a996e463bd3104e72dc324d8d65188
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-05-14 20:11:08 -0600

    Set up GUI

---
+++ PyMultibound/common.py
@@ -0,0 +1,10 @@
+VERSION = "1.0-ALPHA"
+
+
+def runStarbound(profile: str): pass
+
+def createProfile(name: str, imp: bool) -> bool: pass
+
+def deleteProfile(name: str) -> bool: pass
+
+def getProfiles() -> [str]: return ["Vanilla", "Frackin Universe", "Overly Modded"]
--- PyMultibound/gui.py
+++ PyMultibound/gui.py
@@ -2,34 +2,99 @@ import sys
 
 from PyQt5.QtWidgets import *
 
-class Window(QMainWindow):
+from PyMultibound.common import *
+
+
+class MainWindow(QMainWindow):
     """Main Window."""
+
     def __init__(self, parent=None):
         """Initializer."""
         super().__init__(parent)
-        self.setWindowTitle('QMainWindow')
-        self.setCentralWidget(QLabel("I'm the Central Widget"))
-        self._createMenu()
-        self._createToolBar()
-        self._createStatusBar()
+        self.setWindowTitle('PyMultibound')
 
-    def _createMenu(self): pass
+        self.profileList = QListWidget()
+        self._updateProfileList()
+        self.setCentralWidget(self.profileList)
 
-    def _createToolBar(self):
         tools = QToolBar()
         self.addToolBar(tools)
-        tools.addAction('Exit', self.close)
-        tools.addAction("Run Starbound", runStarbound)
+        tools.addAction("Run Starbound", self.attemptRunStarbound)
+        tools.addAction("New Profile", self._newProfileDialog)
+        tools.addAction("Delete Profile", self._deleteProfileDialog)
 
+        self.status = QStatusBar()
+        self.status.showMessage(f"PyMultibound {VERSION}")
+        self.setStatusBar(self.status)
 
-    def _createStatusBar(self):
-        status = QStatusBar()
-        status.showMessage("I'm the Status Bar")
-        self.setStatusBar(status)
+    def _newProfileDialog(self):
+        name, ok = QInputDialog.getText(self, "Create Profile", "Enter Profile Name:")
+        if ok:
+            q = QMessageBox()
+            q.setWindowTitle("Create Profile")
+            q.setText("Import mods from Steam workshop?")
+            q.setIcon(QMessageBox.Question)
+            q.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+
+            workshop = q.exec_() == QMessageBox.Yes
+            if createProfile(name, workshop):
+                self.status.showMessage(f"Created profile {name}!")
+            else:
+                self.status.showMessage("Failed to create profile. Check the log for details.")
+        else:
+            self.status.showMessage("Aborted profile creation")
+
+        self._updateProfileList()
+
+    def _deleteProfileDialog(self):
+        name = self.getSelectedProfile()
+        if name == "":
+            self.status.showMessage("Select a profile before deleting it!")
+            return
+
+        confirm = QMessageBox()
+        confirm.setText(f"Are you sure you want to delete profile {name}?")
+        confirm.setInformativeText("This cannot be undone!")
+        confirm.setWindowTitle("Delete Profile")
+        confirm.setIcon(QMessageBox.Warning)
+        confirm.setStandardButtons(QMessageBox.Cancel | QMessageBox.Yes)
+        if confirm.exec_() == QMessageBox.Yes:
+            if deleteProfile(name):
+                self.status.showMessage(f"Deleted profile {name}")
+            else:
+                self.status.showMessage(f"Failed to delete {name}, check the log for details")
+        else:
+            self.status.showMessage(f"Aborted deletion of {name}")
+
+        self._updateProfileList()
+
+    def attemptRunStarbound(self):
+        name = self.getSelectedProfile()
+        if name == "":
+            self.status.showMessage("Select a profile before starting Starbound!")
+            return
+        runStarbound(name)
+
+    def _updateProfileList(self):
+        self.profileList.clear()
+        for profile in getProfiles():
+            self.profileList.addItem(QListWidgetItem(profile))
+
+    def getSelectedProfile(self) -> str:
+        try:
+            return self.profileList.selectedItems()[0].text()
+        except:
+            info = QMessageBox()
+            info.setWindowTitle("Cannot Complete Operation")
+            info.setText("Select a profile first!")
+            info.setIcon(QMessageBox.Information)
+            info.setStandardButtons(QMessageBox.Ok)
+            info.exec_()
+            return ""
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    win = Window()
+    win = MainWindow()
     win.show()
     sys.exit(app.exec_())
