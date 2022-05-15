bee2a58ef9c1314afcbb5a9d9910c6cc5a3c1030
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-05-15 08:51:07 -0600

    workshop mod support

--- PyMultibound/common.py
+++ PyMultibound/common.py
@@ -68,6 +68,7 @@ if platform.system() == "Windows":
     dumpJson = join(starboundDir, "win32", "dump_versioned_json.exe")  # path to the dump_versioned_json.exe
     makeJson = join(starboundDir, "win32", "make_versioned_json.exe")  # path to the make_versioned_json.exe
     starboundExecutable = join(starboundDir, "win64", "starbound.exe")
+    unpackAssets = join(starboundDir, "win64", "asset_unpacker.exe")
     blankSBInit = blankWindowsSBInit
 
 elif platform.system() == "Linux":
@@ -77,6 +78,7 @@ elif platform.system() == "Linux":
     starboundExecutable = join(starboundDir, "linux", "run-client.sh")
     dumpJson = join(starboundDir, "linux", "dump_versioned_json")  # path to the dump_versioned_json.exe
     makeJson = join(starboundDir, "linux", "make_versioned_json")  # path to the make_versioned_json.exe
+    unpackAssets = join(starboundDir, "linux", "asset_unpacker")
     blankSBInit = blankLinuxSBInit
 
 else:
@@ -117,8 +119,26 @@ def createProfile(name: str, imp: bool) -> bool:
         logging.info(f"Created sbinit.config for {name}")
 
     if not imp: return True
+    # Import steam workshop mods
+    if len(next(os.walk(workshopDir))[1]) > 0:
+        logging.info(f"Found workshop mods. {name}")
+        for modID in next(os.walk(workshopDir))[1]:
+            # Basically for numerically id-ed folder
+            if not os.path.isfile(join(workshopDir, modID, "contents.pak")):
+                logging.warning(f"No contents.pak file was found in workshop mod {modID}")
+            else:
+                logging.debug("Attempting to extract profile metadata")
+                unpacked = unpack(os.path.join(workshopDir, modID, "contents.pak"))
+                modData = loadJson(os.path.join(unpacked, "_metadata"))
+                ["name"]
+                logging.debug("Got mod metadata")
+                shutil.rmtree(unpacked)
 
-
+                safe_move(
+                    join(workshopDir, modID, "contents.pak"),
+                    join(profilesDir, name, "mods", f"workshop-{modData['name']}-{modData['version']}.pak")
+                )
+                logging.info(f"Moved workshop mod {modID} ({modData['name']}) to {name}\"s mod folder")
 
 
 def deleteProfile(name: str) -> bool:
@@ -142,3 +162,22 @@ def getProfiles() -> [str]:
         logging.info("No existing profiles found!")
         pass  # no profiles
     return profiles
+
+
+def unpack(path: str) -> str:
+    logging.info(f"Unpacking {str}")
+    command = f'"{unpackAssets}" "{path}" "{path}-unpacked"'
+    if platform.system() == "Windows": os.system(f'"{command}"')
+    elif platform.system() == "Linux": os.system(command)
+    return f"{path}-unpacked"
+
+def loadJson(path: str) -> {}:
+    logging.debug(f"Loading json from {path}")
+    with open(path, "r") as f:
+        raw = f.read()
+        raw = raw.replace("inf,", "999999999,")  # FU uses "inf" which is invalid, so replace it with a big number
+        # It should automatically overwrite this anyway.
+    return json.loads(raw)
+
+
+def pack(json: str, path:str): pass
