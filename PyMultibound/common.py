29bd8541ae850d82488f6a8e56a1cebeb92ea6fe
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-05-15 19:33:19 -0600

    character editor + documentation

--- PyMultibound/common.py
+++ PyMultibound/common.py
@@ -60,7 +60,11 @@ def safe_move(src, dst):
 
 
 logging.info(f"PyMultibound Version: {VERSION}, Platform: {platform.system()}")
-profilesDir = join(os.path.dirname(os.path.realpath(__file__)), os.pardir, "profiles")  # Directory in which the profiles reside
+profilesDir = join(os.path.dirname(os.path.realpath(__file__)), os.pardir,
+                   "profiles")  # Directory in which the profiles reside
+templatesDir = join(os.path.dirname(os.path.realpath(__file__)), os.pardir, "templates")
+temporaryPath = join(templatesDir, "temp")
+
 if platform.system() == "Windows":
     steamappsDir = os.path.join(*["c:\\", "Program Files (x86)", "Steam", "steamapps"])
     starboundDir = join(steamappsDir, "common", "Starbound")  # Main Starbound directory inside of steamapps
@@ -92,17 +96,22 @@ logging.debug("Starbound Executable" + starboundExecutable)
 
 
 def runStarbound(profile: str):
+    """
+    Run Starbound with the given profile
+    """
     logging.info("Starting starbound...")
     cmd = f'"{starboundExecutable}" ' \
           f'-bootconfig "{join(profilesDir, profile, "sbinit.config")}"'
     logging.info(f"Launch command: {cmd}")
-
-    if platform.system() == "Windows": os.system(f'"{cmd}"')  # Run the game
-    if platform.system() == "Linux": os.system(cmd)  # TODO: find out why windows needs the extra "s
-    # Windows crashes without the "s, and linux crashes with them.
+    runCommand(cmd)
 
 
 def createProfile(name: str, imp: bool) -> bool:
+    """
+    Create a blank profile with the given name.
+
+    imp: whether to import subscribed Steam Workshop mods
+    """
     if os.path.exists(join(profilesDir, name)):
         logging.warning(f"Cannot create profile {name} as it already exists!")
         return False
@@ -130,7 +139,6 @@ def createProfile(name: str, imp: bool) -> bool:
                 logging.debug("Attempting to extract profile metadata")
                 unpacked = unpack(os.path.join(workshopDir, modID, "contents.pak"))
                 modData = loadJson(os.path.join(unpacked, "_metadata"))
-                ["name"]
                 logging.debug("Got mod metadata")
                 shutil.rmtree(unpacked)
 
@@ -142,6 +150,9 @@ def createProfile(name: str, imp: bool) -> bool:
 
 
 def deleteProfile(name: str) -> bool:
+    """
+    Delete the profile with the given name
+    """
     logging.info(f"Deleting profile {name}")
     try:
         shutil.rmtree(join(profilesDir, name))
@@ -153,6 +164,9 @@ def deleteProfile(name: str) -> bool:
 
 
 def getProfiles() -> [str]:
+    """
+    Get the names of every known profile
+    """
     profiles = []
     try:
         for name in next(os.walk(profilesDir))[1]:
@@ -165,13 +179,21 @@ def getProfiles() -> [str]:
 
 
 def unpack(path: str) -> str:
+    """
+    Unpack the .pak file at the given path,
+    and save it to <path>-unpacked/
+    """
     logging.info(f"Unpacking {path}")
-    command = f'"{unpackAssets}" "{path}" "{path}-unpacked"'
-    if platform.system() == "Windows": os.system(f'"{command}"')
-    elif platform.system() == "Linux": os.system(command)
+    runCommand(f'"{unpackAssets}" "{path}" "{path}-unpacked"')
     return f"{path}-unpacked"
 
+
 def loadJson(path: str) -> {}:
+    """
+    Load the json at the given path
+    Automatically replaces some invalid json
+    (FU's "inf" with a large integer, etc.)
+    """
     logging.debug(f"Loading json from {path}")
     with open(path, "r") as f:
         raw = f.read()
@@ -180,4 +202,105 @@ def loadJson(path: str) -> {}:
     return json.loads(raw)
 
 
-def pack(json: str, path:str): pass
+def makeTemplate(characterPath: str) -> bool:
+    """
+    Make a template from the character at characterPath
+    Saves it to templates/<character uuid>.template
+    """
+    logging.info("Attempting to create a new template")
+    character = loadCharacter(characterPath)
+    templatePath = join(templatesDir, f"{character['content']['uuid']}.template")
+    if os.path.exists(templatePath):
+        logging.warning("Could not create template as it would overwrite a template of the same name!")
+        return False
+
+    with open(templatePath, "w+") as f:
+        json.dump(character["content"]["identity"], f, indent=4)
+    logging.info(f"Saved template {templatePath}")
+    return True
+
+
+def loadCharacter(path: str) -> {}:
+    """
+    Load json from the given .player file at path
+    """
+    if os.path.exists(temporaryPath): os.remove(temporaryPath)
+    runCommand(f'"{dumpJson}" "{path}" "{temporaryPath}"')
+    logging.info(f"Created temporary json file from .player at {temporaryPath}")
+    j = loadJson(temporaryPath)
+    os.remove(temporaryPath)
+    return j
+
+
+def saveCharacter(path: str, data: {}):
+    """
+    Save the given data to a .player file at path
+    """
+    with open(temporaryPath, "w") as f:
+        json.dump(data, f, indent=4)
+    runCommand(f'"{makeJson}" "{temporaryPath}" "{path}"')
+
+
+def runCommand(command: str):
+    """
+    Run the specified command.
+    Uses os.system() but formats the command depending on current platform
+    """
+    # Windows crashes without the "s, and linux crashes with them.
+    if platform.system() == "Windows":
+        os.system(f'"{command}"')
+    # TODO: Don't repeat this everywhere (2 in this file, one in main.py)
+    elif platform.system() == "Linux":
+        os.system(command)
+    else:
+        logging.critical(f"Not on Windows or Linux ({platform.system()})")
+
+
+def applyTemplate(templatePath: str, characterPath: str, preserveName: bool = False):
+    """
+    Apply the contents of the template at templatePath
+    to the character at characterPath
+
+    preserveName: whether to preserve the character's name
+    """
+    character = loadCharacter(characterPath)
+    with open(templatePath, "r") as f:
+        template = json.load(f)
+    if preserveName:
+        template["name"] = character["content"]["identity"]["name"]
+
+    character["content"]["identity"] = template
+    saveCharacter(characterPath, character)
+
+
+def getTemplates() -> [str]:
+    """
+    Get the file paths of all available templates
+    """
+    templates = []
+    for template in os.listdir(templatesDir):
+        templates.append((template.replace(".template", ""), join(templatesDir, template)))
+    return templates
+
+
+def getCharacters() -> [(str, str, str)]:
+    """
+    Return a list of all characters across all profiles
+    Each character entry consists of (path, uuid, name)
+    """
+    characters = []
+    try:
+        for name in next(os.walk(profilesDir))[1]:
+            for character in os.listdir(join(profilesDir, name, "storage", "player")):
+                if character.endswith(".player"):
+                    path = join(profilesDir, name, "storage", "player", character)
+                    json = loadCharacter(path)
+                    name = json["content"]["identity"]["name"]
+                    uuid = json["content"]["uuid"]
+                    characters.append((path, uuid, name))
+                    logging.debug(f"Found character {name} ({uuid}) at {path}")
+
+    except StopIteration:
+        logging.info("No existing characters found!")
+
+    return characters
