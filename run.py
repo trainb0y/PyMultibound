7cbb9d211a2d85367c2a014bc53560e17eb1c477
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-05-22 16:15:32 -0600

    External `run.py`

---
+++ run.py
@@ -0,0 +1,14 @@
+# Way to lazy to actually set this up properly...
+import logging
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+from PyMultibound.gui.mainwindow import MainWindow
+
+if __name__ == '__main__':
+    logging.info("Running GUI")
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
